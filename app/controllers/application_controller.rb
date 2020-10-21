19a94c1b625be0cf9002b2c7c9cbe91c4be5cc41
yepes0 <yepes0@chicagobooth.edu>
2020-10-21 19:56:17 +0000

    OmniCalc vF

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-### square root
+### Square 
   def blank_square_form
     render({ :template => "calculation_templates/square_form.html.erb"})
   end
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
     @square_of_num = @num ** 2
     render({ :template => "calculation_templates/square_results.html.erb"})
   end
-### square root
+### Square Root
   def blank_square_root_form
     render({ :template => "calculation_templates/sroot_form.html.erb"})
   end
@@ -17,22 +17,26 @@ class ApplicationController < ActionController::Base
     @square_root_of_sroot = @sroot ** 0.5
     render({ :template => "calculation_templates/sroot_results.html.erb"})
   end
-### payment
+### Payment
   def blank_payment_form
     render({ :template => "calculation_templates/payment_form.html.erb"})
   end
   def calculate_payment
-    @apr = params.fetch("apr").to_f
-    @years = params.fetch("years").to_f
-    @principal = params.fetch("principal").to_f
-    @payment = @sroot ** 0.5
+    @rate = params.fetch("apr").to_f/100
+    @years = params.fetch("term").to_f
+    @principal = params.fetch("amount").to_f
+    @num = @rate/12 * @principal
+    @denom = 1-(1+@rate/12)**(-@years*12)
+    @payment = @num/@denom
+    
     render({ :template => "calculation_templates/payment_results.html.erb"})
   end
 
-
-
-##### help with this later
-  def calculate_random
+##### Random with Form
+def blank_random_form
+    render({ :template => "calculation_templates/rand_form.html.erb"})
+  end  
+def calculate_random
     @lower = params.fetch("user_min").to_f
     @upper = params.fetch("user_max").to_f
     @result = rand(@lower..@upper)
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,28 @@
 Rails.application.routes.draw do
 
+## Square
+
 get("/square/new", { :controller => "application", :action => "blank_square_form" })
 
 get("/square/results", { :controller => "application", :action => "calculate_square" })
+
+## Square Root
 
 get("/square_root/new", { :controller => "application", :action => "blank_square_root_form" })
 
 get("/square_root/results", { :controller => "application", :action => "calculate_square_root" })
 
+## Payment
+
 get("/payment/new", { :controller => "application", :action => "blank_payment_form" })
 
 get("/payment/results", { :controller => "application", :action => "calculate_payment" })
 
-### get("/random/results", { :controller => "application", :action => "calculate_random" })
+## Random
+
+get("/random/new", { :controller => "application", :action => "blank_random_form" })
+
+get("/random/results", { :controller => "application", :action => "calculate_random" })
+
 
 end
